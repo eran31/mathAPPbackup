99dc94325ecb1b4b5cb795dbf705ec444ee34a09
eran31 <eranshimony1@gmail.com>
2022-09-11 11:23:44 +0300

    Update student-list.component.js

--- Client/src/Components/student-list.component.js
+++ Client/src/Components/student-list.component.js
@@ -8,7 +8,8 @@ const [students, setStudents] = useState([]);
 
 useEffect(() => {
 	axios
-	.get("http://localhost:5000/students/")
+	.get("https://mathapp-2len.onrender.com/api/students")
+	//.get("http://localhost:5000/students/")
 	.then(({ data }) => {
 		setStudents(data);
 	})
